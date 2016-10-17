3ba4f51c6558527b8643a16fbd6c23eb652d1b3f
HichamDiouri <hicham_diouri@outlook.com>
2016-10-16 20:37:54 -0400

    Ajout methode point -Sert à ajouter un point avec sont signe -Sa semble fonctionner je te laisse tester par toi meme

--- Dessin.java
+++ Dessin.java
@@ -6,42 +6,52 @@ public class Dessin {
 	 *Ce tableau contient tout les signes
 	 * et leur position respectives.
 	 **/
-	static char [][] dessins = new char[34][18];
+	static char [][] dessin = new char[34][18];
 	
 	/*Methode d'initialisation du canevas*/
 	public static void canevas(){
 		
-		dessins[0][0] = '+';
+		dessin[0][0] = '+';
 		for(int c = 1; c <= 32; c++){
-			dessins[c][0] = '-';
+			dessin[c][0] = '-';
 		}
-		dessins[33][0] = '+';
+		dessin[33][0] = '+';
 		
 		for(int l = 1; l <= 16; l++){
 			for(int c = 0; c <= 33; c++){
 				if(c == 0 || c == 33){
-					dessins[c][l] = '|';
+					dessin[c][l] = '|';
 				}else{
-					dessins[c][l] = ' ';
+					dessin[c][l] = ' ';
 				}
 			}
 		}
 		
-		dessins[0][17] = '+';
+		dessin[0][17] = '+';
 		for(int c = 1; c <= 32; c++){
-			dessins[c][17] = '-';
+			dessin[c][17] = '-';
 		}
-		dessins[33][17] = '+';
+		dessin[33][17] = '+';
 	}
 	
+	/*Methode pour dessiner un point*/
+	public static void point(){
+		char signe = Pep8.chari();
+		int colonne = Pep8.deci();
+		int ligne = Pep8.deci();
+		
+		if(colonne < 32 && colonne >= 0 && ligne < 16 && ligne >= 0){
+			dessin[colonne+1][ligne+1] = signe;
+		}
+	}
 	/*
 	 *Methode d'affichage.
 	 * affiche ce qu'il y a dans le tableau dessin.
 	 **/
 	public static void  affiche(){
-		for(int l = 0; l <= 17; l++){
+		for(int l = 17; l >= 0; l--){
 			for(int c = 0; c <= 33; c++){
-				Pep8.charo(dessins[c][l]);
+				Pep8.charo(dessin[c][l]);
 			}
 			Pep8.charo('\n');
 		}
@@ -58,6 +68,8 @@ public class Dessin {
 				canevas();
 			}else if(commande == 'a'){
 				affiche();
+			}else if(commande == 'p'){
+				point();
 			}
 			commande = Pep8.chari();
 		}
