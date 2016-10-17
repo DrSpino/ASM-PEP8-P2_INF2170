22b543acea633fb6c28c7739c0b587133880fc1e
HichamDiouri <hicham_diouri@outlook.com>
2016-10-16 20:17:07 -0400

    Amelioration du dessin -J'ai preferer utiliser un tableau 2D plus simple -J'ai enlever les modulo trop compliqué à faire en pep/8 -On va galerer pour le pep/8..

--- Dessin.java
+++ Dessin.java
@@ -6,42 +6,44 @@ public class Dessin {
 	 *Ce tableau contient tout les signes
 	 * et leur position respectives.
 	 **/
-	static char [] dessin = new char[631];
+	static char [][] dessins = new char[34][18];
 	
 	/*Methode d'initialisation du canevas*/
 	public static void canevas(){
-		dessin[0] = '+';
-		for(int i = 1; i <= 32; i++){
-			dessin[i] = '-';
-		}
-		dessin[33] = '+';
 		
-		for(int i = 35; i < 596; i++){
-			if((i%35)== 1){
-				dessin[i] = '|';
-			}else if((i%35)== 0){
-				dessin[i] = '\n';
-			}else if((i%35)== 34){
-				dessin[i] = '|';
-			}else{
-				dessin[i] = ' ';
+		dessins[0][0] = '+';
+		for(int c = 1; c <= 32; c++){
+			dessins[c][0] = '-';
+		}
+		dessins[33][0] = '+';
+		
+		for(int l = 1; l <= 16; l++){
+			for(int c = 0; c <= 33; c++){
+				if(c == 0 || c == 33){
+					dessins[c][l] = '|';
+				}else{
+					dessins[c][l] = ' ';
+				}
 			}
 		}
-		dessin[596] = '+';
-		for(int i = 597; i <= 628; i++){
-			dessin[i] = '-';
+		
+		dessins[0][17] = '+';
+		for(int c = 1; c <= 32; c++){
+			dessins[c][17] = '-';
 		}
-		dessin[629] = '+';
-		dessin[630] = '\n';
+		dessins[33][17] = '+';
 	}
 	
 	/*
 	 *Methode d'affichage.
 	 * affiche ce qu'il y a dans le tableau dessin.
 	 **/
-	public static void affiche(){
-		for (int i = 0; i < dessin.length ; i++){
-			Pep8.charo(dessin[i]);
+	public static void  affiche(){
+		for(int l = 0; l <= 17; l++){
+			for(int c = 0; c <= 33; c++){
+				Pep8.charo(dessins[c][l]);
+			}
+			Pep8.charo('\n');
 		}
 	}
 	
